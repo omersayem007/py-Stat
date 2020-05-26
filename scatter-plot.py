ac43dd8d9a9c18fedc189e9ed29ffae283ae7d24
osOmer <omer_sayem@yahoo.com>
2020-05-26 18:48:51 +0600

    Create scatter-plot.py

---
+++ scatter-plot.py
@@ -0,0 +1,13 @@
+# Make a scatter plot
+plt.plot(versicolor_petal_length,versicolor_petal_width,marker='.',linestyle='none')
+
+
+
+# Label the axes
+plt.xlabel('versicolor_petal_length')
+plt.ylabel('versicolor_petal_width')
+
+
+
+# Show the result
+plt.show()
